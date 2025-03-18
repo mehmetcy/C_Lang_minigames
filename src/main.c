d58fc236056180222df83dcd818e7e31d3f98f43
sangarius <61920232+sangarius@users.noreply.github.com>
2025-03-18 22:09:01 +0300

    First initilization of the SDL functions, v0.01

--- src/main.c
+++ src/main.c
@@ -1,20 +1,62 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include "game.h"
+#include <stdio.h>
 
-#include <SDL2/SDL.h>
+#include <SDL.h>
 
-int main(int argc, char *argv[])
+#include "./game.h"
+
+
+int main(int argc, char* argv[])
 {
-        if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) < 0) 
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) 
     {
-        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-        return 1;
+        fprintf(stderr, "Could not initialize sdl2: %s\n", SDL_GetError());
+        return EXIT_FAILURE;
     }
-    //Game loop and other utils
-    printf("Game is about to start...\n");
 
-    //Close the SDL
+    SDL_Window* window = SDL_CreateWindow("Procedural",
+        100, 100,
+        SCREEN_WIDTH, SCREEN_HEIGHT,
+        SDL_WINDOW_SHOWN);
+
+    if (window == NULL) 
+    {
+        fprintf(stderr, "SDL_CreateWindow Error: %s\n", SDL_GetError());
+        return EXIT_FAILURE;
+    }
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1,
+        SDL_RENDERER_ACCELERATED |
+        SDL_RENDERER_PRESENTVSYNC);
+
+    if (renderer == NULL) 
+    {
+        SDL_DestroyWindow(window);
+        fprintf(stderr, "SDL_CreateRenderer Error: %s\n", SDL_GetError());
+        return EXIT_FAILURE;
+    }
+
+    SDL_Event e;
+    int quit = 0;
+    while (!quit) 
+    {
+        while (SDL_PollEvent(&e)) {
+            switch (e.type) {
+            case SDL_QUIT:
+                quit = 1;
+                break;
+
+            default: {}
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyWindow(window);
     SDL_Quit();
-    return 0;
+
+    return EXIT_SUCCESS;
 }
